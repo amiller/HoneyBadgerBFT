d904186c2fc829c393642b4047178437bf2bfc58
Aluex <aluex.cpp@gmail.com>
2015-08-03 11:04:24 -0700

    Minor update

--- mmr13/mmr13_tor_multipleCircuits.py
+++ mmr13/mmr13_tor_multipleCircuits.py
@@ -11,7 +11,7 @@ import mmr13
 reload(mmr13)
 from mmr13 import makeCallOnce, bv_broadcast, shared_coin_dummy, binary_consensus, bcolors, mylog, MVBroadcast, mv84consensus, initBeforeBinaryConsensus
 import stem.control
-import stem.ControllerError
+from stem import ControllerError
 
 import socks
 socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 9050, True)
@@ -195,7 +195,7 @@ def random_delay_multivalue_consensus(N, t, inputs):
                 circuit_id = controller.new_circuit([random.choice(nodesList), random.choice(
                     nodesList)], await_build=True)
                 break
-            except stem.ControllerError:
+            except ControllerError:
                 print "Requesting Circuit Failed. Re-Trying..."
                 pass
         circuit_ids.append(circuit_id)
