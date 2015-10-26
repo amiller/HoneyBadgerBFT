55b28c9a0de8ad6980dcbf294ed67d61ce49978b
Aluex <aluex.cpp@gmail.com>
2015-10-26 20:57:19 +0800

    update

--- mmr13/honest_party_test.py
+++ mmr13/honest_party_test.py
@@ -1,4 +1,6 @@
+#!/usr/bin/python
 __author__ = 'aluex'
+
 
 from gevent.queue import Queue
 from gevent import Greenlet
@@ -11,7 +13,16 @@ import gevent
 import os
 #import random
 from utils import myRandom as random
+import fcp
+import json
+import cPickle as pickle
+import time
+import zlib
 #print state
+import base64
+import socks
+import struct
+from io import BytesIO
 
 nameList = ["Alice", "Bob", "Christina", "David", "Eco", "Francis", "Gerald", "Harris", "Ive", "Jessica"]
 
@@ -26,7 +37,135 @@ def randomTransaction():
     tx.amount = random.randint(1, 100)
     return tx
 
-def client_test_random_delay(N, t):
+def randomTransactionStr():
+    return repr(randomTransaction())
+
+msgCounter = 0
+starting_time = dict()
+ending_time = dict()
+msgSize = dict()
+msgFrom = dict()
+msgTo = dict()
+msgContent = dict()
+logChannel = Queue()
+
+def logWriter(fileHandler):
+    while True:
+        msgCounter, msgSize, msgFrom, msgTo, st, et, content = logChannel.get()
+        fileHandler.write("%d:%d(%d->%d)[%s]-[%s]%s\n" % (msgCounter, msgSize, msgFrom, msgTo, st, et, content))
+        fileHandler.flush()
+
+class deepEncodeException(Exception):
+    pass
+
+class deepDecodeException(Exception):
+    pass
+
+def encodeTransaction(tr):
+    sourceInd = nameList.index(tr.source)
+    targetInd = nameList.index(tr.target)
+    return struct.pack(
+        '<BBH', sourceInd, targetInd, tr.amount
+    )
+
+def deepEncode(mc, m):
+    buf = BytesIO()
+    buf.write(struct.pack('<I', mc))
+    f, t, (tag, c) = m
+    buf.write(struct.pack('BB', f, t))
+    # totally we have 4 msg types
+    if c[0]=='i':
+        buf.write('\x01')
+        t2, p1, s = c
+        buf.write(struct.pack('B', p1))
+        for tr in s:
+            buf.write(encodeTransaction(tr))
+    elif c[0]=='e':
+        buf.write('\x02')
+        t2, p1, (p2, s) = c
+        buf.write(struct.pack('BB', p1, p2))
+        for tr in s:
+            buf.write(encodeTransaction(tr))
+    else:
+        p1, (t2, (p2, p3)) = c
+        if t2 == 'B':
+            buf.write('\x03')
+        elif t2 == 'A':
+            buf.write('\x04')
+        else:
+            raise deepEncodeException()
+        buf.write(struct.pack('BBB', p1, p2, p3))
+    buf.seek(0)
+    return buf.read()
+
+def constructTransactionFromRepr(r):
+    sourceInd, targetInd, amount = struct.unpack('<BBH', r)
+    tr = Transaction()
+    tr.source = nameList[sourceInd]
+    tr.target = nameList[targetInd]
+    tr.amount = amount
+    return tr
+
+# Msg Types:
+# 1:(3, 1, ('B', ('i', 1, set([{{Transaction from Francis to Eco with 86}}]))))
+# 2:(1, 0, ('B', ('e', 0, (2, set([{{Transaction from Bob to Jessica with 65}}])))))
+# 3:(0, 3, ('A', (1, ('B', (1, 1)))))
+# 4:(0, 3, ('A', (2, ('A', (1, 1)))))
+
+def deepDecode(m):
+    buf = BytesIO(m)
+    mc, f, t, msgtype = struct.unpack('<IBBB', buf.read(7))
+    trSet = set()
+    if msgtype == 1:
+        p1, = struct.unpack('B', buf.read(1))
+        trRepr = buf.read(4)
+        while trRepr:
+            trSet.add(constructTransactionFromRepr(trRepr))
+            trRepr = buf.read(4)
+        return mc, (f, t, ('B', ('i', p1, trSet)),)
+    elif msgtype == 2:
+        p1, p2 = struct.unpack('BB', buf.read(2))
+        trRepr = buf.read(4)
+        while trRepr:
+            trSet.add(constructTransactionFromRepr(trRepr))
+            trRepr = buf.read(4)
+        return mc, (f, t, ('B', ('e', p1, (p2, trSet))),)
+    elif msgtype == 3:
+        p1, p2, p3 = struct.unpack('BBB', buf.read(3))
+        return mc, (f, t, ('A', (p1, ('B', (p2, p3)))),)
+    elif msgtype == 4:
+        p1, p2, p3 = struct.unpack('BBB', buf.read(3))
+        return mc, (f, t, ('A', (p1, ('A', (p2, p3)))),)
+    else:
+        raise deepDecodeException()
+
+def encode(m):  # TODO
+    global msgCounter
+    msgCounter += 1
+    starting_time[msgCounter] = str(time.time())  # time.strftime('[%m-%d-%y|%H:%M:%S]')
+    #intermediate = deepEncode(msgCounter, m)
+    result = deepEncode(msgCounter, m)
+    #result = zlib.compress(
+    #    #pickle.dumps(deepEncode(msgCounter, m)),
+    #    intermediate,
+    #9)  # Highest compression level
+    #print 'intermediateLen', len(intermediate), 'compressed', len(result)
+    msgSize[msgCounter] = len(result)
+    msgFrom[msgCounter] = m[1]
+    msgTo[msgCounter] = m[0]
+    msgContent[msgCounter] = m
+    return result
+
+def decode(s):  # TODO
+    result = deepDecode(s)
+    #result = deepDecode(zlib.decompress(s)) #pickle.loads(zlib.decompress(s))
+    assert(isinstance(result, tuple))
+    ending_time[result[0]] = str(time.time())  # time.strftime('[%m-%d-%y|%H:%M:%S]')
+    msgContent[result[0]] = None
+    logChannel.put((result[0], msgSize[result[0]], msgFrom[result[0]], msgTo[result[0]], starting_time[result[0]], ending_time[result[0]], result[1]))
+    return result[1]
+
+def client_test_freenet(N, t):
     '''
     Test for the client with random delay channels
 
@@ -41,35 +180,11 @@ def client_test_random_delay(N, t):
     :return None:
     '''
     maxdelay = 0.01
-
     buffers = map(lambda _: Queue(1), range(N))
-    parser = defaultdict(lambda _: lambda _: None)
-
-    def it(tokens): # include transaction
-        target = int(tokens[1])
-        if len(tokens)==2:
-            controlChannels[target].put(('IncludeTransaction', randomTransaction()))
-        else:
-            exception("Bad Arguments")
-    def halt(tokens): # halt
-        target = int(tokens[1])
-        if len(tokens)==2:
-            controlChannels[target].put(('Halt', None))
-        else:
-            exception("Bad Arguments")
-    def msg(tokens):
-        target = int(tokens[1])
-        if len(tokens)==3:
-            controlChannels[target].put(('Msg', tokens[2]))
-        else:
-            exception("Bad Arguments")
-    def help(tokens):
-        mylog("%s\n" % client_test_random_delay.__doc__)
-
-    parser["it"] = parser["i"] = it
-    parser["halt"] = parser["h"] = halt
-    parser["msg"] = parser["m"] = msg
-    parser["help"] = help
+    gtemp = Greenlet(logWriter, open('msglog.TorMultiple', 'w'))
+    gtemp.parent_args = (N, t)
+    gtemp.name = 'client_test_freenet.logWriter'
+    gtemp.start()
 
     # Instantiate the "broadcast" instruction
     def makeBroadcast(i):
@@ -77,29 +192,36 @@ def client_test_random_delay(N, t):
             def _deliver(j):
                 #print 'Delivering', v, 'from', i, 'to', j
                 # mylog(bcolors.OKGREEN + "MSG: [%d] -> [%d]: %s" % (i, j, repr(v)) + bcolors.ENDC)
-                buffers[j].put((i,v))
+                buffers[j].put(encode((i, v)))
                 # mylog(bcolors.OKGREEN + "     [%d] -> [%d]: Finish" % (i, j) + bcolors.ENDC)
             for j in range(N):
                 Greenlet(_deliver, j).start_later(random.random()*maxdelay)
         return _broadcast
 
-    while True:
+    def recvWithDecode(buf):
+        def recv():
+            s = buf.get()
+            return decode(s)
+        return recv
+
+    #while True:
+    if True:
         initBeforeBinaryConsensus()
         ts = []
         controlChannels = [Queue() for _ in range(N)]
         for i in range(N):
             bc = makeBroadcast(i)
-            recv = buffers[i].get
+            recv = recvWithDecode(buffers[i])
             th = Greenlet(honestParty, i, N, t, controlChannels[i], bc, recv)
             controlChannels[i].put(('IncludeTransaction', randomTransaction()))
             th.start_later(random.random() * maxdelay)
             ts.append(th)
 
-        def monitorUserInput(): # No idea why raw_input will block the whole gevent, need some investigation
-            while True:
-                mylog(">>>")
-                tokens = [s for s in raw_input().strip().split() if s]
-                mylog("= %s\n" % repr(parser[tokens[0]](tokens)))
+        #def monitorUserInput(): # No idea why raw_input will block the whole gevent, need some investigation
+        #    while True:
+        #        mylog(">>>")
+        #        tokens = [s for s in raw_input().strip().split() if s]
+        #        mylog("= %s\n" % repr(parser[tokens[0]](tokens)))
 
         #Greenlet(monitorUserInput).start()
         try:
@@ -109,8 +231,47 @@ def client_test_random_delay(N, t):
         except gevent.hub.LoopExit: # Manual fix for early stop
             print "Concensus Finished"
             mylog(bcolors.OKGREEN + ">>>" + bcolors.ENDC)
-            tokens = [s for s in raw_input().strip().split() if s]
-            mylog("= %s\n" % repr(parser[tokens[0]](tokens)))  # In case the parser has an output
+            #tokens = [s for s in raw_input().strip().split() if s]
+            #mylog("= %s\n" % repr(parser[tokens[0]](tokens)))  # In case the parser has an output
+
+import GreenletProfiler
+import atexit
+import gc
+import traceback
+from greenlet import greenlet
+
+USE_PROFILE = False
+
+
+def exit():
+    halfmsgCounter = 0
+    for msgindex in starting_time.keys():
+        if msgindex not in ending_time.keys():
+            logChannel.put((msgindex, msgSize[msgindex], msgFrom[msgindex],
+                msgTo[msgindex], starting_time[msgindex], time.time(), '[UNRECEIVED]' + repr(msgContent[msgindex])))
+            halfmsgCounter += 1
+    mylog('%d extra log exported.' % halfmsgCounter, verboseLevel=-1)
+
+    for ob in gc.get_objects():
+        if not hasattr(ob, 'parent_args'):
+            continue
+        if not ob:
+            continue
+        if not ob.exception:
+            continue
+        mylog('%s[%s] called with parent arg\n(%s)\n%s' % (ob.name, repr(ob.args), repr(ob.parent_args),
+            ''.join(traceback.format_stack(ob.gr_frame))), verboseLevel=-1)
+
+    if USE_PROFILE:
+        GreenletProfiler.stop()
+        stats = GreenletProfiler.get_func_stats()
+        stats.print_all()
+        stats.save('profile.callgrind', type='callgrind')
 
 if __name__ == '__main__':
-    client_test_random_delay(5, 1)
+    GreenletProfiler.set_clock_type('cpu')
+    atexit.register(exit)
+    if USE_PROFILE:
+        GreenletProfiler.start()
+    client_test_freenet(5, 1)
+
