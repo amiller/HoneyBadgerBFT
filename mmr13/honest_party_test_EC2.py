dde00c966e4d3679a37e63162845b55206fe1074
Aluex <aluex.cpp@gmail.com>
2015-11-03 01:43:33 +0800

    quick fix

--- mmr13/honest_party_test_EC2.py
+++ mmr13/honest_party_test_EC2.py
@@ -319,7 +319,7 @@ def client_test_freenet(N, t):
             th = Greenlet(honestParty, i, N, t, controlChannels[i], bc, recv)
             th.parent_args = (N, t)
             th.name = 'client_test_freenet.honestParty(%d)' % i
-            controlChannels[i].put(('IncludeTransaction', randomTransactionStr()))
+            controlChannels[i].put(('IncludeTransaction', randomTransaction()))
             th.start()
             mylog('Summoned party %i at time %f' % (i, time.time()), verboseLevel=-1)
             ts.append(th)
