50177797ee44e74827ce46fd1505e5b43dec04cc
Aluex <aluex.cpp@gmail.com>
2015-11-03 00:54:44 +0800

    Update

--- mmr13/honest_party_test_EC2.py
+++ mmr13/honest_party_test_EC2.py
@@ -93,6 +93,7 @@ IP_LIST = []
 def prepareIPList(content):
     global IP_LIST
     IP_LIST = content.split('\n')  # getAddrFromEC2Summary(content)
+    print IP_LIST
 
 # TOR_MAPPINGS = [(host, BASE_PORT+i) for i, host in enumerate(TOR_MAPPING_LIST)]
 IP_MAPPINGS = [(host, BASE_PORT) for i, host in enumerate(IP_LIST)]
