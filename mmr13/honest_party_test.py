ba9cb84b75b5dc3ef3c5fe30d2af1ce6be05dea0
Aluex <aluex.cpp@gmail.com>
2015-11-06 19:36:33 +0800

    Update to let detector ignore healthy greenlets

--- mmr13/honest_party_test.py
+++ mmr13/honest_party_test.py
@@ -165,7 +165,7 @@ def client_test_freenet(N, t):
             for item in logChannel:
                 mylog(item, verboseLevel=-1)
             mylog("=====", verboseLevel=-1)
-            checkExceptionPerGreenlet()
+            #checkExceptionPerGreenlet()
             # print getSignatureCost()
 
         print "Concensus Finished"
