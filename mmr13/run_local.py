4b48a5d471cad675517f371c7693982772689033
Aluex <aluex.cpp@gmail.com>
2015-11-08 22:17:07 +0800

    update

--- mmr13/run_local.py
+++ mmr13/run_local.py
@@ -12,7 +12,8 @@ def runOnTransaction(N, t, Tx):
     while True:
         line = p.stdout.readline()
         if 'size' in line:
-            print Tx, line
+            # print Tx, line
+            return line.replace('Total Message size ','').strip()
         if line == '':
             break
         # print(line.strip())  # remove extra ws between lines
@@ -25,8 +26,11 @@ def runOnTransaction(N, t, Tx):
 
 def main(N, t):
     for i in range(11):
+        line = [str(2**i)]
         for j in range(4):
-            runOnTransaction(N, t, 2**i)
+            line.append(runOnTransaction(N, t, 2**i))
+        print ','.join(line)
+    
 
 if __name__=='__main__':
     main(int(sys.argv[1]), int(sys.argv[2]))
