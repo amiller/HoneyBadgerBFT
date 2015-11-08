1f4caa9191a437b23d139a50325770e3a28f518d
Aluex <aluex.cpp@gmail.com>
2015-11-09 00:03:29 +0800

    adjust verbose level

--- mmr13/includeTransaction.py
+++ mmr13/includeTransaction.py
@@ -227,7 +227,7 @@ lock.put(1)
 @greenletFunction
 def honestParty(pid, N, t, controlChannel, broadcast, receive):
     # RequestChannel is called by the client and it is the client's duty to broadcast the tx it wants to include
-    mylog("timestampB (%d, %lf)" % (pid, time.time()), verboseLevel=-1)
+    mylog("timestampB (%d, %lf)" % (pid, time.time()), verboseLevel=-2)
     #sock = socket.create_connection((sys.argv[4], 51234))
     transactionCache = set()
     sessionID = 0
@@ -255,7 +255,7 @@ def honestParty(pid, N, t, controlChannel, broadcast, receive):
             transactionCache = transactionCache.difference(syncedTXSet)
             #mylog("[%d] synced transactions %s, now cached %s" % (pid, repr(syncedTXSet), repr(transactionCache)), verboseLevel = -1)
             mylog("[%d] synced transactions." % pid, verboseLevel = -2)
-            mylog("timestampE (%d, %lf)" % (pid, time.time()), verboseLevel=-1)
+            mylog("timestampE (%d, %lf)" % (pid, time.time()), verboseLevel=-2)
             lock.get()
             finishcount += 1
             lock.put(1)
