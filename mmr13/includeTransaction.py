1d554150e3cbfd1328346d1b66d7b9255605fbf7
Aluex <aluex.cpp@gmail.com>
2015-11-09 00:40:32 +0800

    update

--- mmr13/includeTransaction.py
+++ mmr13/includeTransaction.py
@@ -96,6 +96,8 @@ def multiSigBr(pid, N, t, msg, broadcast, receive, outputs):
                     buf.seek(0)
                     step = 4 * len(msgBundle[2]) % (N - t) == 0 and 4 * len(msgBundle[2]) / (N - t) or (4 * len(msgBundle[2]) % (N - t) + 1)
                     fragList = [buf.read(step) for i in range(N - t)]
+                    if len(fragList[-1]) < step:
+                        fragList[-1] = fragList[-1] + '\xFF' * (step - len(fragList[-1]))
                     newBundle = (msgBundle[1], zfecEncoder.encode(fragList)[msgBundle[1]])
                     #newBundle = (msgBundle[1], msgBundle[2])
                     #mylog("[%d] we are to echo msgBundle: %s" % (pid, repr(msgBundle)), verboseLevel=-1)
@@ -124,7 +126,7 @@ def multiSigBr(pid, N, t, msg, broadcast, receive, outputs):
                         #except:
                         #    raise ECDSASignatureError()  # just a place holder
                         # outputs[originBundle[0]].put(originBundle[1])
-                            outputs[originBundle[0]].put(reconstruction)
+                            outputs[originBundle[0]].put(reconstruction.rstrip('\xFF'))
                 else:
                     raise ECDSASignatureError()
 
