07f45c421e88c6421e5073abd9b5b4a59f2b46f2
Aluex <aluex.cpp@gmail.com>
2015-11-10 19:35:16 +0800

    fix run_local

--- mmr13/run_local_tor.py
+++ mmr13/run_local_tor.py
@@ -1,9 +1,10 @@
-import subprocess, sys, signal
+__author__ = 'aluex'
 
+import subprocess, sys, signal
+#./honest_party_test_tor_multipleCircuits.py . 4_1.key ecdsa_keys 1 4 1
 def runOnTransaction(N, t, Tx):
     p = subprocess.Popen(
-        ['python', './honest_party_test.py', '%d_%d.key' % (N, t), 'ecdsa_keys', '%d' % Tx, str(N), str(t)],
-        shell=False,
+        ['python', './honest_party_test_tor_multipleCircuits.py', 'lol', '%d_%d.key' % (N, t), 'ecdsa_keys', '%d' % Tx, str(N), str(t)],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         stdin=subprocess.PIPE
@@ -23,21 +24,22 @@ def runOnTransaction(N, t, Tx):
         if counter >= N - t and not sent:
             p.send_signal(signal.SIGINT)
             sent = True
-            print 'signal sent'
+            # print 'signal sent'
         # print line, counter
+    q = subprocess.check_output(['python', 'process.py', 'msglog.TorMultiple'])
+    print q.replace('\n', ' ')
 
-    
 import sys
 def main(N, t, start_i=0, end_i=11, start_j=0):
     for i in range(start_i, end_i):
         sys.stdout.write(str(2**i))
         for j in range(start_j, 4):
-            sys.stdout.write(' ' + str(runOnTransaction(N, t, 2**i)))
+            runOnTransaction(N, t, 2**i)
         sys.stdout.write('\n')
 
 
 if __name__=='__main__':
     if len(sys.argv) > 3:
-        main(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]), int(sys.argv[5]))    
+        main(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]), int(sys.argv[5]))
     else:
         main(int(sys.argv[1]), int(sys.argv[2]))
