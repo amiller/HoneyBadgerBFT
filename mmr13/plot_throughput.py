4f6f081b1af8883ed84ee9a178f25f1ca9bf8689
Aluex <aluex.cpp@gmail.com>
2015-11-12 01:56:30 +0800

    update

--- mmr13/plot_throughput.py
+++ mmr13/plot_throughput.py
@@ -1,50 +1,82 @@
-from pylab import *
+import matplotlib.pyplot as plt
 
 # Measurements from the table
 expt = [
     (8,2,[
-        (512, 3.918, 2.849),
-        (1024, 4.764, 3.740),
-        (4096, 6.228, 5.108),
-        (8192, 10.337, 9.128),
-        (16384, 19.396, 18.705),
-        (32768, 37.852, 36.618),
+        # (512,5.96925),
+        (1024,6.41475),
+        (2048,6.402),
+        (4096,7.75725),
+        (8192,10.99675),
+        (16384,18.7975),
+        (32768,36.5285),
+        (65536,70.299),
+        (131072, 138.395),
+        (262144, 295.122)
     ]),
     (16,4,[
-        (1024,5.581,4.260),
-        (2048,6.893,5.685),
-        (4096,10.552,9.294),
-        (8192,21.945,18.719),
-        (16384,39.207,36.931)
+        (512, 5.6815),
+        (1024, 6.40175),
+        (2048, 8.33675),
+        (4096, 12.37275),
+        (8192, 20.29125),
+        (16384, 39.1535),
+        (32768, 73.4515),
+        (65536, 140.214)
     ]),
     (32,8,[
-        (1024,8.599,7.152),
-        (2048,13.139,11.434),
-        (4096,22.405,20.767),
-        (8192,46.093,43.609)
+        (512, 9.489),
+        (1024, 11.5725),
+        (2048, 15.89175),
+        (4096, 24.24975),
+        (8192, 43.201),
+        (16384, 79.7425),
+        (32768, 153.85975)
     ]),
-    (64,16, [
-        (512,19.670,14.531),
-        (1024,22.933,19.871),
-        (2048,53.125,29.110),
-        (4096,81.278,55.204),
-        (8192,171.143,158.949),
+    (64, 16,[
+        (512, 24.3315),
+        (1024, 32.17575),
+        (2048, 39.399625),
+        (4096, 58.59675),
+        (8192, 96.832),
+        (16384, 173.94425)
+    ]),
+    (64, 21, [
+        (512, 26.027),
+        (1024, 31.5315),
+        (2048, 42.291),
+        (4096, 63.3355),
+        (8192, 105.0055),
+        (16384, 190.3235)
+    ]),
+    (128, 32, [
+        (256, 89.2895),
+        (512, 94.507),
+        (1024, 105.0365)
     ])]
 
 
 
 def do_plot():
-    close(1)
-    figure(1, figsize=(7,5));
-    clf()
+    f = plt.figure(1, figsize=(7,5));
+    plt.clf()
+    ax = f.add_subplot(1, 1, 1)
     for N,t, entries in expt:
         throughput = []
         batch = []
-        for ToverN, _, latency in entries:
-            batch.append(N*ToverN/1000.)
+        for ToverN, latency in entries:
+            batch.append(N*ToverN)
             throughput.append(ToverN*(N-t) / latency)
-        plot(batch, throughput, label='%d/%d' % (N,t))
-    legend(title='Nodes/tolerance', loc='best')
-    ylabel('Throughput (Tx per second)')
-    xlabel('Requests  (Tx x1000)')
-    tight_layout()
+        ax.plot(batch, throughput, label='%d/%d' % (N,t))
+    ax.set_xscale("log")
+    ax.set_yscale("log")
+    plt.ylim([10**2.1, 10**3.8])
+    plt.xlim([10**3.8, 10**6.1])
+    plt.legend(title='Nodes / Tolerance', loc='best')
+    plt.ylabel('Throughput (Tx per second)')
+    plt.xlabel('Requests (Tx)')
+    # plt.tight_layout()
+    # plt.show()
+    plt.savefig('plot_throughput.svg', format='svg', dpi=1000)
+
+do_plot()
