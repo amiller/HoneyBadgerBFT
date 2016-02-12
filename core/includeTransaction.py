18803aa275c7f05cd27121466562dd0ed22c594a
Aluex <aluex.cpp@gmail.com>
2016-02-12 06:32:10 -0500

    update

--- core/includeTransaction.py
+++ core/includeTransaction.py
@@ -70,7 +70,7 @@ def multiSigBr(pid, N, t, msg, broadcast, receive, outputs):
             mt[i + treeLength] = someHash(strList[i])  # TODO: need to change strList[i] from a string to an integer here.
         for i in range(treeLength-1, 0, -1):  # 1, 2, 3, ..., treeLength - 1
             mt[i] = someHash(mt[i*2] ^ mt[i*2+1])  # XOR is commutative
-        return
+        return mt
 
     def getMerkleBranch(index, mt):
         res = []
