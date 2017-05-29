80f09d15beaa73b9ee91eed0abd9ca8a6fa5d3d1
Andrew Miller <socrates1024@gmail.com>
2017-05-29 00:10:02 -0500

    Added RBC module and tests

---
+++ core/reliablebroadcast.py
@@ -0,0 +1,231 @@
+# coding=utf-8
+from collections import defaultdict
+import zfec
+import hashlib
+
+#### zfec encode ####
+def encode(K, N, m):
+    """Erasure encodes string m into N blocks, such that any K
+    can reconstruct.
+    """
+    encoder = zfec.Encoder(K, N)
+    assert K <= 256  # TODO: Record this assumption!
+    # pad m to a multiple of K bytes
+    padlen = K - (len(m) % K)
+    m += padlen * chr(K-padlen)
+    step = len(m)/K
+    blocks = [m[i*step : (i+1)*step] for i in range(K)]
+    stripes = encoder.encode(blocks)
+    return stripes
+
+def decode(K, N, stripes):
+    """Decodes an erasure-encoded string from a subset of stripes
+
+    @param stripes : a container of N elements,
+        each of which is either a string or None
+        at least K elements are strings
+        all string elements are the same length
+    """
+    assert len(stripes) == N
+    blocks = []
+    blocknums = []
+    for i,block in enumerate(stripes):
+        if block is None: continue
+        blocks.append(block)
+        blocknums.append(i)
+        if len(blocks) == K: break
+    else: raise ValueError("Too few to recover")
+    decoder = zfec.Decoder(K, N)
+    rec = decoder.decode(blocks, blocknums)
+    m = ''.join(rec)
+    padlen = K - ord(m[-1])
+    m = m[:-padlen]
+    return m
+    
+#### Merkle tree ####
+def hash(x):
+    assert type(x) is str
+    return hashlib.sha256(x).digest()
+
+def ceil(x): return int(math.ceil(x))
+
+def merkleTree(strList):
+    """Builds a merkle tree from a list of N strings (N at least 1)
+
+    Returns tree, an array of 2*ceil(N) strings
+    The root digest is at tree[1], tree[0] is blank
+    """
+    N = len(strList)
+    assert N >= 1
+    bottomrow = 2 ** ceil(math.log(N, 2))
+    mt = [""] * (2 * bottomrow)
+    for i in range(N):
+        mt[bottomrow + i] = hash(strList[i])
+    for i in range(bottomrow - 1, 0, -1):
+        mt[i] = hash(mt[i*2] + mt[i*2+1])
+    return mt
+
+def getMerkleBranch(index, mt):
+    """Computes a merkle tree from a list of leaves.
+    """
+    res = []
+    t = index + (len(mt) >> 1)
+    while t > 1:
+        res.append(mt[t ^ 1])  # we are picking up the sibling
+        t /= 2
+    return res
+
+def merkleVerify(N, val, roothash, branch, index):
+    """Verify a merkle tree branch proof
+    """
+    assert 0 <= index < N
+    assert type(val) is str
+    assert len(branch) == ceil(math.log(N, 2))
+    # Index has information on whether we are facing a left sibling or a right sibling            
+    tmp = hash(val)
+    tindex = index
+    for br in branch:
+        tmp = hash((tindex & 1) and br + tmp or tmp + br)
+        tindex >>= 1
+    if tmp != roothash:
+        print "Verification failed with", hash(val), roothash, branch, tmp == roothash
+        return False
+    return True
+
+
+def reliablebroadcast(pid, N, f, leader, input, receive, send):
+    """Reliable broadcast
+
+    Args
+    ----
+    pid : 0 <= pid < N
+    N   : int, at least 3
+    f   : fault tolerance, N >= 3f+1
+    leader : 0 <= leader < N
+    input  : if pid == leader, then input() is call to wait for the input value
+    receive : receive() blocks until a message is received
+       Message is of the form:
+       (i, (tag, ...)) = receive()
+       tag is one of {"VAL", "ECHO", "READY"}
+    send: sends a message to a designed recipient
+       send(i, (tag, ...))
+
+    Messages
+    -----
+    VAL( roothash, branch[i], stripe[i] )
+        sent from Leader to each other party
+    ECHO( roothash, branch[i], stripe[i] )
+        sent after receiving VAL message
+    READY( roothash )
+        sent after receiving N-f ECHO messages
+        or after receiving f+1 Ready Messages 
+
+    returns m after receiving 2f+1 READY messages and N-2f ECHO messages
+
+    Accountability 
+    --------------
+    TODO: A large computational expense occurs when attempting to
+    decode the value from erasure codes, and recomputing to check it
+    is formed correctly. By transmitting a signature along with VAL
+    and ECHO, we can ensure that if the value is decoded but not
+    necessarily reconstructed, then evidence incriminates the leader.
+
+    """
+    assert N <= 3*f + 1
+    assert f >= 0
+    assert 0 <= leader < N
+    assert 0 <= pid    < N
+
+    K               = N - 2 * f  # Need this many to reconstruct
+    EchoThreshold   = N - f      # Wait for this many ECHO to send READY
+    ReadyThreshold  = f + 1      # Wait for this many READY to amplify READY
+    OutputThreshold = 2 * f + 1  # Wait for this many READY to output
+    # NOTE: The above thresholds  are chosen to minimize the size
+    # of the erasure coding stripes, i.e. to maximize K.
+    # The following alternative thresholds are more canonical
+    # (e.g., in Bracha '86) and require larger stripes, but must wait
+    # for fewer nodes to respond
+    #   EchoThreshold = ceil((N + f + 1.)/2)
+    #   K = EchoThreshold - f    
+
+    def broadcast(o):
+        for i in range(N): send(i, o)
+        
+    if pid == leader:
+        # The leader erasure encodes the input, sending one strip to each participant
+        m = input()  # block until an input is received
+        assert type(m) is str
+        #print 'Input received: %d bytes' % (len(m),)
+        
+        stripes = encode(K, N, m)
+        mt = merkleTree(stripes)  # full binary tree
+        roothash = mt[1]
+
+        for i in range(N):
+            branch = getMerkleBranch(i, mt)  
+            send(i, ('VAL', roothash, branch, stripes[i]))
+
+    # TODO: filter policy: if leader, discard all messages until sending VAL
+
+    fromLeader = None
+    stripes = defaultdict(lambda: [None for _ in range(N)])
+    echoCounter = defaultdict(lambda: 0)
+    readyCounter = defaultdict(lambda: 0)
+    readySent = False
+
+    def decode_output(roothash):
+        # Rebuild the merkle tree to guarantee decoding is correct
+        m = decode(K, N, stripes[roothash])
+        _stripes = encode(K, N, m)
+        _mt = merkleTree(_stripes)
+        _roothash = _mt[1]
+        # TODO: Accountability: If this fails, incriminate leader
+        assert _roothash == roothash
+        return m
+
+    while True:  # main receive loop
+        sender, msg = receive()
+        if msg[0] == 'VAL' and fromLeader is None:
+            (_, roothash, branch, stripe) = msg
+            if sender != leader:
+                print "VAL message from other than leader:", sender
+                continue
+            try: assert merkleVerify(N, stripe, roothash, branch, pid)
+            except e:
+                print "Failed to validate VAL message:", e
+                continue
+            fromLeader = roothash
+            broadcast(('ECHO', roothash, branch, stripe ))
+
+        elif msg[0] == 'ECHO':
+            (_, roothash, branch, stripe) = msg
+            if roothash in stripes and stripes[roothash][sender] is not None:
+                print "Redundant ECHO"
+                continue
+            try: assert merkleVerify(N, stripe, roothash, branch, sender)
+            except e:
+                print "Failed to validate ECHO message:", e
+                continue
+
+            # Store this stripe
+            stripes[roothash][sender] = stripe
+            echoCounter[roothash] += 1
+
+            if echoCounter[roothash] >= EchoThreshold and not readySent:
+                readySent = True
+                broadcast(('READY', roothash))
+
+            if readyCounter[roothash] >= OutputThreshold and echoCounter[roothash] >= K:
+                return decode_output(roothash)
+
+        elif msg[0] == 'READY':
+            (_, roothash) = msg
+            readyCounter[roothash] += 1
+
+            # Amplify ready messages
+            if readyCounter[roothash] >= ReadyThreshold and not readySent:
+                readySent = True
+                broadcast(('READY', roothash))
+
+            if readyCounter[roothash] >= OutputThreshold and echoCounter[roothash] >= K:
+                return decode_output(roothash)
---
+++ test/test_rbc.py
@@ -0,0 +1,123 @@
+from gevent import Greenlet
+from gevent.queue import Queue
+import gevent
+import random
+import core.reliablebroadcast as rbc
+from rbc import reliablebroadcast, encode, decode
+
+### Merkle tree
+
+def test_merkletree0():
+    mt = merkleTree(["hello"])
+    assert mt == ["", hash("hello")]
+
+def test_merkletree1():
+    strList = ["hello","hi","ok"]
+    mt = merkleTree(strList)
+    roothash = mt[1]
+    assert len(mt) == 8
+    for i in range(3):
+        val = strList[i]
+        branch = getMerkleBranch(i, mt)
+        assert merkleVerify(3, val, roothash, branch, i)
+
+### Zfec
+
+def test_zfec1():
+    K = 3
+    N = 10
+    m = "hello this is a test string"
+    stripes = encode(K, N, m)
+    assert decode(K, N, stripes) == m
+    _s = list(stripes)
+    # Test by setting some to None
+    _s[0] = _s[1] = _s[4] = _s[5] = _s[6] = None
+    assert decode(K, N, _s) == m
+    _s[3] = _s[7] = None
+    assert decode(K, N, _s) == m
+    # _s[8] = None
+    # assertRaises(ValueError, decode, K, N, _s)
+
+
+### RBC
+
+def simple_router(N, maxdelay=0.01, seed=None):
+    """Builds a set of connected channels, with random delay
+    @return (receives, sends)
+    """
+    rnd = random.Random(seed)
+    if seed is not None: print 'ROUTER SEED: %f' % (seed,)
+    
+    queues = [Queue() for _ in range(N)]
+    
+    def makeSend(i):
+        def _send(j, o):
+            delay = rnd.random() * maxdelay
+            #print 'SEND %8s [%2d -> %2d] %.2f' % (o[0], i, j, delay)
+            gevent.spawn_later(delay, queues[j].put, (i,o))
+            #queues[j].put((i, o))
+        return _send
+    
+    def makeRecv(j):
+        def _recv():
+            (i,o) = queues[j].get()
+            #print 'RECV %8s [%2d -> %2d]' % (o[0], i, j)
+            return (i,o)
+        return _recv
+        
+    return ([makeSend(i) for i in range(N)],
+            [makeRecv(j) for j in range(N)])
+    
+def test_rbc1(N=4, f=1, leader=None, seed=None):
+    # Test everything when runs are OK
+    if seed is not None: print 'SEED:', seed
+    rnd = random.Random(seed)
+    router_seed = rnd.random()
+    if leader is None: leader = rnd.randint(0,N-1)
+    sends, recvs = simple_router(N, seed=seed)
+    threads = []
+    leader_input = Queue(1)
+    for i in range(N):
+        input = leader_input.get if i == leader else None
+        t = Greenlet(reliablebroadcast, i, N, f, leader, input, recvs[i], sends[i])
+        t.start()
+        threads.append(t)
+
+    m = "Hello! This is a test message."
+    leader_input.put(m)
+    gevent.joinall(threads)
+    assert [t.value for t in threads] == [m]*N
+
+def test_rbc2(N=4, f=1, leader=None, seed=None):
+    # Crash up to f nodes
+    if seed is not None: print 'SEED:', seed
+    rnd = random.Random(seed)
+    router_seed = rnd.random()
+    if leader is None: leader = rnd.randint(0,N-1)
+    sends, recvs = simple_router(N, seed=router_seed)
+    threads = []
+    leader_input = Queue(1)
+
+    for i in range(N):
+        input = leader_input.get if i == leader else None
+        t = Greenlet(reliablebroadcast, i, N, f, leader, input, recvs[i], sends[i])
+        t.start()
+        threads.append(t)
+
+    m = "Hello!asdfasdfasdfasdfasdfsadf"
+    leader_input.put(m)
+    gevent.sleep(0)  # Let the leader get out its first message
+    
+    # Crash f of the nodes
+    crashed = set()
+    print 'Leader:', leader
+    for _ in range(f):
+        i = rnd.choice(range(N))
+        crashed.add(i)
+        threads[i].kill()
+        threads[i].join()
+    print 'Crashed:', crashed
+    gevent.joinall(threads)
+    print [t.value for t in threads]
+    for i,t in enumerate(threads):
+        if i not in crashed: assert t.value == m
