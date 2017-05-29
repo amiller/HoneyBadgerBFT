9f7803b385dd70492b1de9868c9d94f9fcfc2be9
Andrew Miller <socrates1024@gmail.com>
2017-05-29 14:59:40 -0500

    Now with binary agreement and tests

---
+++ core/binaryagreement.py
@@ -0,0 +1,131 @@
+import gevent
+from gevent.event import Event
+from collections import defaultdict
+
+def binaryagreement(pid, N, f, coin, input, decide, broadcast, receive):
+    '''Binary consensus from [MMR14]. It takes an input vi and will finally write the decided value into _decide_ channel.
+    :param pid: my id number
+    :param N: the number of parties
+    :param f: the number of byzantine parties
+    :param coin: a common coin(r) is called to block until receiving a bit
+    :param input: input() is called to receive an input
+    :param decide: decide(0) or output(1) is eventually called
+    :param broadcast: broadcast channel
+    :param receive: receive channel
+    :return: blocks until 
+    '''
+    # Messages received are routed to either a shared coin, the broadcast, or AUX
+    est_values = defaultdict(lambda:[set(),set()])
+    aux_values = defaultdict(lambda:[set(),set()])
+    est_sent = defaultdict(lambda:[False,False])
+    bin_values = defaultdict(set)
+
+    # This event is triggered whenever bin_values or aux_values changes
+    bv_signal = Event()
+
+    def _recv():
+        while True:  #not finished[pid]:
+            (sender, msg) = receive()
+            assert sender in range(N)
+            if msg[0] == 'EST':
+                # BV_Broadcast message
+                _, r, v = msg
+                assert v in (0,1)
+                if sender in est_values[r][v]:
+                    print 'Redundant EST received', msg
+                    raise
+                    continue
+
+                est_values[r][v].add(sender)
+                # Relay after reaching first threshold
+                if len(est_values[r][v]) >= f + 1 and not est_sent[r][v]:
+                    est_sent[r][v] = True
+                    broadcast(('EST', r, v))
+
+                # Output after reaching second threshold
+                if len(est_values[r][v]) >= 2 * f + 1:
+                    bin_values[r].add(v)
+                    bv_signal.set()
+
+            elif msg[0] == 'AUX':
+                # Aux message
+                _, r, v = msg
+                assert v in (0,1)
+                if sender in aux_values[r][v]:
+                    print 'Redundant AUX received', msg
+                    raise
+                    continue
+
+                aux_values[r][v].add(sender)
+                bv_signal.set()
+
+    # Translate mmr14 broadcast into coin.broadcast
+    #_coin_broadcast = lambda (r, sig): broadcast(('COIN', r, sig))
+    #_coin_recv = Queue()
+    #coin = shared_coin(sid+'COIN', pid, N, f, _coin_broadcast, _coin_recv.get)
+
+    # Run the receive loop in the background
+    _thread_recv = gevent.spawn(_recv)
+
+    # Block waiting for the input
+    vi = input()
+    assert vi in (0,1)
+    est = vi
+    r = 0
+    already_decided = None
+    while True:  # Unbounded number of rounds
+        if not est_sent[r][est]:
+            est_sent[r][est] = True
+            broadcast(('EST', r, est))
+
+        while len(bin_values[r]) == 0:
+            # Block until a value is output
+            bv_signal.clear()
+            bv_signal.wait()
+
+        w = next(iter(bin_values[r]))  # take an element
+        broadcast(('AUX', r, w))
+
+        values = None
+        while True:
+            # Block until at least N-f AUX values are received
+            if 1 in bin_values[r] and len(aux_values[r][1]) >= N - f:
+                values = set((1,))
+                #print 'VALUES 1'
+                break
+            if 0 in bin_values[r] and len(aux_values[r][0]) >= N - f:
+                values = set((0,))
+                #print 'VALUES 0'
+                break
+            if sum(len(aux_values[r][v]) for v in bin_values[r]) >= N - f:
+                values = set((0,1))
+                #print 'VALUES BOTH'
+                break
+            bv_signal.clear()
+            bv_signal.wait()
+
+        # Block until receiving the common coin value
+        s = coin(r)
+
+        if len(values) == 1:
+            v = next(iter(values))
+            if v == s:
+                if already_decided is None:
+                    already_decided = v
+                    decide(v)
+                    #print pid, 'DECIDED', v, 'in round', r
+                elif already_decided == v:
+                    # Here corresponds to a proof that if one party
+                    # decides at round r, then in all the following
+                    # rounds, everybody will propose r as an
+                    # estimation. (Lemma 2, Lemma 1) An abandoned
+                    # party is a party who has decided but no enough
+                    # peers to help him end the loop.  Lemma: # of
+                    # abandoned party <= t
+                    #print pid, 'QUITTING', r, 'in round', r
+                    _thread_recv.kill()
+                    return
+                est = v
+        else:
+            est = s
+        r += 1
---
+++ test/test_binaryagreement.py
@@ -0,0 +1,149 @@
+import unittest
+import gevent
+import random
+from gevent.event import Event
+from gevent.queue import Queue
+from core.commoncoin import shared_coin
+import core.binaryagreement
+reload(core.binaryagreement)
+from core.binaryagreement import binaryagreement
+from crypto.threshsig.boldyreva import dealer
+from collections import defaultdict
+
+def simple_broadcast_router(N, maxdelay=0.005, seed=None):
+    """Builds a set of connected channels, with random delay
+    @return (receives, sends)
+    """
+    rnd = random.Random(seed)
+    #if seed is not None: print 'ROUTER SEED: %f' % (seed,)
+    
+    queues = [Queue() for _ in range(N)]
+    _threads = []
+
+    def makeBroadcast(i):
+        def _send(j, o):
+            delay = rnd.random() * maxdelay
+            #print 'SEND   %8s [%2d -> %2d] %2.1f' % (o[0], i, j, delay*1000), o[1:]
+            gevent.spawn_later(delay, queues[j].put, (i,o))
+            #queues[j].put((i, o))
+        def _bc(o):
+            print 'BCAST  %8s [%2d ->  *]' % (o[0], i), o[1]
+            for j in range(N): _send(j, o)
+        return _bc
+
+    def makeRecv(j):
+        def _recv():
+            (i,o) = queues[j].get()
+            #print 'RECV %8s [%2d -> %2d]' % (o[0], i, j)
+            return (i,o)
+        return _recv
+        
+    return ([makeBroadcast(i) for i in range(N)],
+            [makeRecv(j)      for j in range(N)])
+
+def dummy_coin(sid, N, f):
+    counter = defaultdict(int)
+    events = defaultdict(Event)
+    def getCoin(round):
+        # Return a pseudorandom number depending on the round, without blocking
+        counter[round] += 1
+        if counter[round] == f+1: events[round].set()
+        events[round].wait()
+        return hash((sid,round)) % 2
+    return getCoin
+
+### Test binary agreement with a dummy coin
+def _test_binaryagreement_dummy(N=4, f=1, seed=None):
+    # Generate keys
+    sid = 'sidA'    
+    # Test everything when runs are OK
+    #if seed is not None: print 'SEED:', seed
+    rnd = random.Random(seed)
+    router_seed = rnd.random()
+    sends, recvs = simple_broadcast_router(N, seed=seed)
+
+    threads = []
+    inputs = []
+    outputs = []
+    coin = dummy_coin(sid, N, f)  # One dummy coin function for all nodes
+
+    for i in range(N):
+        inputs.append(Queue(1))
+        outputs.append(Queue(1))
+        
+        t = gevent.spawn(binaryagreement, i, N, f, coin,
+                         inputs[i].get, outputs[i].put, sends[i], recvs[i])
+        threads.append(t)
+
+    for i in range(N):
+        inputs[i].put(random.randint(0,1))
+    #gevent.killall(threads[N-f:])
+    #gevent.sleep(3)
+    #for i in range(N-f, N):
+    #    inputs[i].put(0)
+    try:
+        outs = [outputs[i].get() for i in range(N)]
+        assert len(set(outs)) == 1
+        try: gevent.joinall(threads)
+        except gevent.hub.LoopExit: pass
+    except KeyboardInterrupt:
+        gevent.killall(threads)
+        raise
+
+def test_binaryagreement_dummy():
+    _test_binaryagreement_dummy()
+
+### Test binary agreement with boldyreva coin
+def _make_coins(sid, N, f, seed):
+    # Generate keys
+    PK, SKs = dealer(N, f+1)
+    rnd = random.Random(seed)
+    router_seed = rnd.random()
+    sends, recvs = simple_broadcast_router(N, seed=seed)
+    coins = [shared_coin(sid, i, N, f, PK, SKs[i], sends[i], recvs[i]) for i in range(N)]
+    return coins
+
+def _test_binaryagreement(N=4, f=1, seed=None):
+    # Generate keys
+    sid = 'sidA'
+    # Test everything when runs are OK
+    #if seed is not None: print 'SEED:', seed
+    rnd = random.Random(seed)
+
+    # Instantiate the common coin
+    coins_seed = rnd.random()
+    coins = _make_coins(sid+'COIN', N, f, coins_seed)
+
+    # Router
+    router_seed = rnd.random()
+    sends, recvs = simple_broadcast_router(N, seed=seed)
+
+    threads = []
+    inputs = []
+    outputs = []
+
+    for i in range(N):
+        inputs.append(Queue(1))
+        outputs.append(Queue(1))
+        
+        t = gevent.spawn(binaryagreement, i, N, f, coins[i],
+                         inputs[i].get, outputs[i].put, sends[i], recvs[i])
+        threads.append(t)
+
+    for i in range(N):
+        inputs[i].put(random.randint(0,1))
+    #gevent.killall(threads[N-f:])
+    #gevent.sleep(3)
+    #for i in range(N-f, N):
+    #    inputs[i].put(0)
+    try:
+        outs = [outputs[i].get() for i in range(N)]
+        assert len(set(outs)) == 1
+        try: gevent.joinall(threads)
+        except gevent.hub.LoopExit: pass
+    except KeyboardInterrupt:
+        gevent.killall(threads)
+        raise
+
+def test_binaryagreement():
+    for i in range(5): _test_binaryagreement(seed=i)
