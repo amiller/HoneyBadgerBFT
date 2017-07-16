f6973339ff477c61992a3ce5eee5f5838e014fb9
Andrew Miller <socrates1024@gmail.com>
2017-07-16 12:11:58 -0400

    Corrected previous commit, forgot new files

---
+++ honeybadgerbft/core/honeybadger.py
@@ -0,0 +1,164 @@
+import gevent
+from gevent.event import Event
+from gevent.queue import Queue
+from honeybadgerbft.core.commoncoin import shared_coin
+from honeybadgerbft.core.binaryagreement import binaryagreement
+from honeybadgerbft.core.reliablebroadcast import reliablebroadcast
+from honeybadgerbft.core.commonsubset import commonsubset
+from honeybadgerbft.core.honeybadger_block import honeybadger_block
+from honeybadgerbft.crypto.threshenc import tpke
+
+class HoneyBadgerBFT():
+
+    def __init__(self, sid, pid, B, N, f, sPK, sSK, ePK, eSK, send, recv):
+        self.sid = sid
+        self.pid = pid
+        self.B = B
+        self.N = N
+        self.f = f
+        self.sPK = sPK
+        self.sSK = sSK
+        self.ePK = ePK
+        self.eSK = eSK
+        self._send = send
+        self._recv = recv
+
+        self.round = 0  # Current block number
+        self.transaction_buffer = []
+        self._per_round_recv = {}  # Buffer of incoming messages
+
+
+    def submit_tx(self, tx):
+        print 'submit_tx', self.pid, tx
+        self.transaction_buffer.append(tx)
+
+    def run(self):
+        def _recv():
+            while True:
+                (sender, (r, msg)) = self._recv()
+            
+                # Maintain an *unbounded* recv queue for each epoch
+                if r not in self._per_round_recv:
+                    # Buffer this message
+                    assert r >= round
+                    self._per_round_recv[r] = Queue()
+
+                _recv = self._per_round_recv[r]
+                if _recv is None:
+                    # We have already closed this
+                    # round and will stop participating!
+                    pass
+                else:
+                    # Queue it
+                    _recv.put( (sender, msg) )
+        self._recv_thread = gevent.spawn(_recv)
+
+        while True:
+            # For each round...
+            r = self.round
+            if r not in self._per_round_recv:
+                self._per_round_recv[r] = Queue()
+
+            # Select all the transactions (TODO: actual random selection)
+            tx_to_send = self.transaction_buffer[:self.B]
+
+            # TODO: Wait a bit if transaction buffer is not full
+
+            # Run the round
+            def _make_send(r):
+                def _send(j, o):
+                    self._send(j, (r, o))
+                return _send
+            send_r = _make_send(r)
+            recv_r = self._per_round_recv[r].get
+            new_tx = self._run_round(r, tx_to_send[0], send_r, recv_r)
+            print 'new_tx:', new_tx
+
+            # Remove all of the new transactions from the buffer
+            self.transaction_buffer = [_tx for _tx in self.transaction_buffer if _tx not in new_tx]
+
+            self.round += 1 # Increment the round
+            if self.round >= 3: break # Only run one round for now
+
+
+    def _run_round(self, r, tx_to_send, send, recv):
+        # Unique sid for each round
+        sid = self.sid + ':' + str(r)
+        pid = self.pid
+        N = self.N
+        f = self.f
+
+        def broadcast(o):
+            for j in range(N): send(j, o)
+
+        # Launch ACS, ABA, instances
+        coin_recvs = [None] * N
+        aba_recvs  = [None] * N
+        rbc_recvs  = [None] * N
+
+        aba_inputs  = [Queue(1) for _ in range(N)]
+        aba_outputs = [Queue(1) for _ in range(N)]
+        rbc_outputs = [Queue(1) for _ in range(N)]
+
+        my_rbc_input = Queue(1)
+        print pid, r, 'tx_to_send:', tx_to_send
+
+        def _setup(j):
+            def coin_bcast(o):
+                broadcast(('ACS_COIN', j, o))
+
+            coin_recvs[j] = Queue()
+            coin = shared_coin(sid + 'COIN' + str(j), pid, N, f,
+                               self.sPK, self.sSK,
+                               coin_bcast, coin_recvs[j].get)
+
+            def aba_bcast(o):
+                broadcast(('ACS_ABA', j, o))
+
+            aba_recvs[j] = Queue()
+            aba = gevent.spawn(binaryagreement, sid+'ABA'+str(j), pid, N, f, coin,
+                               aba_inputs[j].get, aba_outputs[j].put_nowait,
+                               aba_bcast, aba_recvs[j].get)
+
+            def rbc_send(k, o):
+                send(k, ('ACS_RBC', j, o))
+
+            # Only leader gets input
+            rbc_input = my_rbc_input.get if j == pid else None
+            rbc_recvs[j] = Queue()
+            rbc = gevent.spawn(reliablebroadcast, sid+'RBC'+str(j), pid, N, f, j,
+                               rbc_input, rbc_recvs[j].get, rbc_send)
+            rbc_outputs[j] = rbc.get  # block for output from rbc
+
+        # N instances of ABA, RBC
+        for j in range(N): _setup(j)
+
+        # One instance of TPKE
+        def tpke_bcast(o):
+            broadcast(('TPKE', 0, o))
+
+        tpke_recv = Queue()
+
+        # One instance of ACS
+        acs = gevent.spawn(commonsubset, pid, N, f, rbc_outputs,
+                           [_.put_nowait for _ in aba_inputs],
+                           [_.get for _ in aba_outputs])
+        
+        def _recv():
+            while True:
+                (sender, (tag, j, msg)) = recv()
+                if   tag == 'ACS_COIN': coin_recvs[j].put_nowait((sender,msg))
+                elif tag == 'ACS_RBC' : rbc_recvs [j].put_nowait((sender,msg))
+                elif tag == 'ACS_ABA' : aba_recvs [j].put_nowait((sender,msg))
+                elif tag == 'TPKE'    : tpke_recv.put_nowait((sender,msg))
+                else:
+                    print 'Unknown tag!!', tag
+                    raise
+        gevent.spawn(_recv)
+
+        _input = Queue(1)
+        _input.put(tx_to_send)
+        return honeybadger_block(pid, self.N, self.f, self.ePK,self. eSK,
+                                 _input.get,
+                                 acs_in=my_rbc_input.put_nowait, acs_out=acs.get,
+                                 tpke_bcast=tpke_bcast, tpke_recv=tpke_recv.get)
---
+++ test/test_honeybadger.py
@@ -0,0 +1,91 @@
+import unittest
+import gevent
+import random
+from gevent.event import Event
+from gevent.queue import Queue
+import honeybadgerbft.core.honeybadger
+reload(honeybadgerbft.core.honeybadger)
+from honeybadgerbft.core.honeybadger import HoneyBadgerBFT
+from honeybadgerbft.crypto.threshsig.boldyreva import dealer
+from honeybadgerbft.crypto.threshenc import tpke
+from collections import defaultdict
+
+def simple_router(N, maxdelay=0.005, seed=None):
+    """Builds a set of connected channels, with random delay
+    @return (receives, sends)
+    """
+    rnd = random.Random(seed)
+    #if seed is not None: print 'ROUTER SEED: %f' % (seed,)
+    
+    queues = [Queue() for _ in range(N)]
+    _threads = []
+
+    def makeSend(i):
+        def _send(j, o):
+            delay = rnd.random() * maxdelay
+            #delay = 0.1
+            #print 'SEND   %8s [%2d -> %2d] %2.1f' % (o[0], i, j, delay*1000), o[1:]
+            gevent.spawn_later(delay, queues[j].put_nowait, (i,o))
+        return _send
+
+    def makeRecv(j):
+        def _recv():
+            (i,o) = queues[j].get()
+            #print 'RECV %8s [%2d -> %2d]' % (o[0], i, j)
+            return (i,o)
+        return _recv
+        
+    return ([makeSend(i) for i in range(N)],
+            [makeRecv(j) for j in range(N)])
+
+
+### Test asynchronous common subset
+def _test_honeybadger(N=4, f=1, seed=None):
+    sid = 'sidA'
+    # Generate threshold sig keys
+    sPK, sSKs = dealer(N, f+1, seed=seed)
+    # Generate threshold enc keys
+    ePK, eSKs = tpke.dealer(N, f+1)
+    
+    rnd = random.Random(seed)
+    #print 'SEED:', seed
+    router_seed = rnd.random()
+    sends, recvs = simple_router(N, seed=router_seed)
+
+    badgers = [None] * N
+    threads = [None] * N
+    for i in range(N):
+        badgers[i] = HoneyBadgerBFT(sid, i, 1, N, f,
+                                    sPK, sSKs[i], ePK, eSKs[i],
+                                    sends[i], recvs[i])
+        threads[i] = gevent.spawn(badgers[i].run)
+
+    for i in range(N):
+        #if i == 1: continue
+        badgers[i].submit_tx('<[HBBFT Input %d]>' % i)
+
+    for i in range(N):
+        badgers[i].submit_tx('<[HBBFT Input %d]>' % (i+10))
+
+    for i in range(N):
+        badgers[i].submit_tx('<[HBBFT Input %d]>' % (i+20))
+        
+    #gevent.killall(threads[N-f:])
+    #gevent.sleep(3)
+    #for i in range(N-f, N):
+    #    inputs[i].put(0)
+    try:
+        outs = [threads[i].get() for i in range(N)]
+
+        # Consistency check
+        assert len(set(outs)) == 1
+        
+    except KeyboardInterrupt:
+        gevent.killall(threads)
+        raise
+
+from nose2.tools import params
+
+def test_honeybadger():
+    _test_honeybadger()
+
