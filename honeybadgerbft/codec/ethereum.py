d1c0ab616554359e3b701de14d7443ae74d16d9c
Andrew Miller <socrates1024@gmail.com>
2017-07-18 17:37:05 -0400

    unyank

--- honeybadgerbft/codec/ethereum.py
+++ honeybadgerbft/codec/ethereum.py
@@ -1,1 +1,9 @@
 from ethereum.transactions import Transaction
+import rlp
+from rlp.sedes import CountableList
+
+def decode(payload):
+    return rlp.decode(payload, CountableList(Transaction))
+
+def encode(txes):
+    return rlp.encode(txes)
