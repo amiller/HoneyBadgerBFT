76102e3b50acd5ff67ba1a3dc07c9409a7a52360
Sylvain Bellemare <sbellem@gmail.com>
2017-10-18 23:04:30 +0200

    Add test for ciphertext generation

--- test/crypto/threshenc/test_tpke.py
+++ test/crypto/threshenc/test_tpke.py
@@ -1,6 +1,8 @@
+from base64 import decodestring
 from random import shuffle
 
 from Crypto.Hash import SHA256
+from charm.toolbox.pairinggroup import PairingGroup, ZR, G1, G2, pair
 
 
 def test_tpke():
@@ -22,3 +24,46 @@ def test_tpke():
         S = set(SS[:PK.k])
         m_ = PK.combine_shares(C, dict((s, shares[s]) for s in S))
         assert m_ == m
+
+
+def test_ciphertext_generation():
+    from honeybadgerbft.crypto.threshenc.tpke import TPKEPublicKey
+    players = 10
+    k = 5
+    group = PairingGroup('SS512')
+    ZERO = group.random(ZR)*0
+    ONE = group.random(ZR)*0 + 1
+    g1 = group.hash('geng1', G1)
+    g1.initPP()
+    g2 = g1
+
+    coefficients = [group.random(ZR) for _ in range(k)]
+    secret = coefficients[0]
+
+    # Polynomial evaluation
+    def f(x):
+        y = ZERO
+        xx = ONE
+        for coeff in coefficients:
+            y += coeff * xx
+            xx *= x
+        return y
+
+    # Shares of master secret key
+    SKs = [f(i) for i in range(1, players+1)]
+    assert f(0) == secret
+
+    # Verification keys
+    VK = g2 ** secret
+    VKs = [g2 ** xx for xx in SKs]
+
+    public_key = TPKEPublicKey(players, k, VK, VKs)
+    public_key.encrypt
+    message = SHA256.new('abc123').digest()
+    ciphertext = public_key.encrypt(message)
+    U, V, W = ciphertext
+
+    assert len(V) == 32
+    UV = decodestring(group.serialize(U)[2:]) + V
+    H = group.hash(UV, G2)
+    assert pair(g1, W) == pair(U, H)
